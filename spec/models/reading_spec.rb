7eda8a1801ebe1270958f380323ec8349936e336
Ahmed Umer Anees <ahmed.anees@iqwadev.com>
2021-12-04 20:33:04 +0500

    Extra code removed

--- spec/models/reading_spec.rb
+++ spec/models/reading_spec.rb
@@ -6,9 +6,7 @@ RSpec.describe 'Reading Get request',  :type => :request do
       thermostat = Thermostat.first
       reading = thermostat.readings.first
       headers = { "ACCEPT" => "application/json", "token" => thermostat.household_token  }
-      get "api/v1/readings/#{reading.id}", :params => { :reading => {
-        number: rand(11.2...76.9),temperature: rand(11.2...76.9), humidity: rand(11.2...76.9),battery_charge: rand(11.2...76.9)
-       } }, :headers => headers
+      get "api/v1/readings/#{reading.id}", :params => { }, :headers => headers
       json = JSON.parse(response.body)
       expect(json["status"]).to eq('success')
     end
